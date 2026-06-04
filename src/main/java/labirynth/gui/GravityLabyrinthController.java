553df3814870952fd5c2e281394e5ae031137efe
Szabo Marcell <szabo-marcell@mailbox.unideb.hu>
2026-06-05 01:11:38 +0200

    feat: creating the initialize() and initializeGrid() methods in the GravityLabyrinthController.java

--- src/main/java/labirynth/gui/GravityLabyrinthController.java
+++ src/main/java/labirynth/gui/GravityLabyrinthController.java
@@ -1,4 +1,57 @@
 package labirynth.gui;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import labirynth.model.LabirynthState;
+import org.pmw.tinylog.Logger;
+
+
 public class GravityLabyrinthController {
+    @FXML
+    private GridPane board;
+    @FXML
+    private Button downButton;
+    @FXML
+    private Button upButton;
+    @FXML
+    private Button rightButton;
+    @FXML
+    private Button leftButton;
+    @FXML
+    private TextField numberOfMovesField;
+    private LabirynthState state;
+    @FXML
+    private Label numberOfMovesLabel;
+
+    private final IntegerProperty numberOfMoves = new SimpleIntegerProperty();
+
+    private final StringProperty playerName =  new SimpleStringProperty();
+
+    public void setPlayerName(String playerName) {
+        this.playerName.set(playerName);
+    }
+    @FXML
+    private void initialize() {
+        initializeGrid();
+    }
+
+    private void initializeGrid() {
+        Logger.debug("Initializing board");
+        for (var i = 0; i < board.getRowCount(); i++) {
+            for (var j = 0; j < board.getColumnCount(); j++) {
+                var square = new StackPane();
+            }
+        }
+    }
+
+
+
 }
